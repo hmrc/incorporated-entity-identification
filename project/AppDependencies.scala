1a8363142c5ceada54dc46fa3dcaddfd1fd93e06
Rob Hawkes <17453872+kabuumu@users.noreply.github.com>
2021-08-12 13:35:20 +0100

    Remove trailing comma

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "5.10.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-27",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-27"
   )
 
   val test = Seq(
