9f09d16da8e9c92ae3cbbbc479683a4a2e638b85
woollett-r <55831974+woollett-r@users.noreply.github.com>
2022-08-24 12:29:41 +0100

    SAR-10302 Update bootstrap play and other dependencies

--- it/utils/ComponentSpecHelper.scala
+++ it/utils/ComponentSpecHelper.scala
@@ -71,13 +71,13 @@ trait ComponentSpecHelper extends AnyWordSpec with Matchers
   }
 
   def get[T](uri: String): WSResponse = {
-    await(buildClient(uri).withHttpHeaders().get)
+    await(buildClient(uri).withHttpHeaders("Authorization" -> "Bearer123").get)
   }
 
   def post[T](uri: String)(body: T)(implicit writes: Writes[T]): WSResponse = {
     await(
       buildClient(uri)
-        .withHttpHeaders("Content-Type" -> "application/json")
+        .withHttpHeaders("Content-Type" -> "application/json", "Authorization" -> "Bearer123")
         .post(writes.writes(body).toString())
     )
   }
@@ -85,13 +85,13 @@ trait ComponentSpecHelper extends AnyWordSpec with Matchers
   def put[T](uri: String)(body: T)(implicit writes: Writes[T]): WSResponse = {
     await(
       buildClient(uri)
-        .withHttpHeaders("Content-Type" -> "application/json")
+        .withHttpHeaders("Content-Type" -> "application/json", "Authorization" -> "Bearer123")
         .put(writes.writes(body).toString())
     )
   }
 
   def delete[T](uri: String): WSResponse = {
-    await(buildClient(uri).delete())
+    await(buildClient(uri).withHttpHeaders("Authorization" -> "Bearer123").delete())
   }
 
   val baseUrl: String = "/incorporated-entity-identification"
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,20 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.18.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"  % "0.70.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "7.1.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"  % "0.71.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.18.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.1.0" % Test,
     "org.scalatest" %% "scalatest" % "3.2.9" % "test, it",
     "com.typesafe.play" %% "play-test" % current % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.29.1" % IntegrationTest,
-    "org.mockito" %% "mockito-scala" % "1.16.37" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.16.37" % Test,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.5" % "test, it",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.70.0" % Test
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.31.0" % IntegrationTest,
+    "org.mockito" %% "mockito-scala" % "1.16.55" % Test,
+    "org.mockito" %% "mockito-scala-scalatest" % "1.16.55" % Test,
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.7" % "test, it",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.71.0" % Test
   )
 }
